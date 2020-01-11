12673b21ebf810619b2a6fdbf4f637a510620b24
Balaji <vbalaji215@gmail.com>
2020-01-10 19:06:45 -0500

    updated HelloController

--- src/main/java/com/hello/controller/HelloWorldController.java
+++ src/main/java/com/hello/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.hello.controller;
 
 import com.hello.entity.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World Bean !!!");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+
+        return new HelloWorldBean(String.format("Hello, %s",name));
     }
 }
